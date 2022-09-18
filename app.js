37a739bf43a2e2eed9c516ae1fa4375911d7607b
edgar_regalado <edgar.regalado@g-global.com>
2022-09-18 15:06:42 -0700

    fixed static file sending

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.use('/media', express.static(__dirname + '/media'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    return res.json({ message: "ok" });
-    // return res.sendFile(__dirname + '/public' + 'index.html')
+    return res.sendFile(__dirname + '/public' + 'index.html')
 });
 
 app.listen(process.env.PORT, `${process.env.HOST}`, () => {
